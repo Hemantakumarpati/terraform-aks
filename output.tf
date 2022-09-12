3604d6ce20d948c485da5d12d2ee75a402188a73
Hemantakumarpati <34638115+Hemantakumarpati@users.noreply.github.com>
2022-09-12 11:59:47 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,8 +1,6 @@
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.k8s.kube_config_raw
-  sensitive = true
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate
 }
-
-output "get_credentials" {
-  value = "az aks get-credentials --name ${var.cluster_name} --resource-group ${var.resource_group_name} --admin -f -"
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
 }
