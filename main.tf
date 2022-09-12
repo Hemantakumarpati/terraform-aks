911df0318e4509cbf9860a9d973baa8ae8eff634
Hemantakumarpati <34638115+Hemantakumarpati@users.noreply.github.com>
2022-09-12 11:56:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,30 @@
-resource "azurerm_resource_group" "k8s" {
-    name     = var.resource_group_name
-    location = var.location
+provider "azurerm" {
+  version = "=1.43.0"
 }
 
-resource "azurerm_kubernetes_cluster" "k8s" {
-    name                = var.cluster_name
-    location            = azurerm_resource_group.k8s.location
-    resource_group_name = azurerm_resource_group.k8s.name
-    dns_prefix          = var.dns_prefix
+resource "azurerm_resource_group" "resource_group" {
+  name     = var.resource_group_name
+  location = var.location
+}
 
-    default_node_pool {
-        name                = "default"
-        node_count          = var.node_count
-        vm_size             = var.vm_size
-        enable_auto_scaling = false
-    }
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = var.cluster_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  dns_prefix          = var.dns_prefix
 
-    identity {
-        type = "SystemAssigned"
-    }
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.node_size
+  }
 
-    azure_policy_enabled = false
+  service_principal {
+    client_id     = var.service_principal.client_id
+    client_secret = var.service_principal.client_secret
+  }
 
-    http_application_routing_enabled = false
-
-    tags = {
-        Environment = "Development"
-    }
+  tags = {
+    Environment = var.environment
+  }
 }
