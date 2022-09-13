dcb501f53bca22054adae8f52508313d47680658
Hemantakumarpati <34638115+Hemantakumarpati@users.noreply.github.com>
2022-09-13 15:51:41 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,14 +8,14 @@ variable "location" {
   default = "Central India"
 }
 variable "node_count" {
-  default = 2
+ default = 2
 }
 variable "node_size" {
   default = "Standard_D2_v2"
 }
 variable "service_principal" {
   type = object({
-    client_id     = string,
+    client_id     = string
     client_secret = string
   })
   description = "The service principal to use"
