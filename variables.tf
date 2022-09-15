da9c1ff37791a976ac8411bad31a4b280944ff99
Hemantakumarpati <34638115+Hemantakumarpati@users.noreply.github.com>
2022-09-15 17:57:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,6 +13,13 @@ variable "node_count" {
 variable "node_size" {
   default = "Standard_D2_v2"
 }
+variable "service_principal" {
+  type = object({
+    client_id     = string
+    client_secret = string
+  })
+  description = "The service principal to use"
+}
 
 variable dns_prefix {
     default = "aks-cluster-001"
